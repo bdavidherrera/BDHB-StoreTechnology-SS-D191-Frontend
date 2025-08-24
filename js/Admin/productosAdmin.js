9678cb4360399d76d3962b06787d2104e6626cc6
Tu Nombre <bherrerabarajs@gmail.com>
2025-08-24 18:06:07 -0500

    Herrera Barajas 2 24-08-2028 commit

--- js/Admin/productosAdmin.js
+++ js/Admin/productosAdmin.js
@@ -34,7 +34,7 @@ async function obtenerProductos() {
                 <td>${nombreProducto}</td>
                 <td>
                     <div class="rollo-imagen-container">
-                        <img src="img/${imagen}" width="40px" class="rollo-imagen">
+                        <img src="/img/${imagen}" width="40px" class="rollo-imagen">
                     </div>
                 </td>
                 <td>$${Number(valor).toLocaleString('es-CO')}</td>
